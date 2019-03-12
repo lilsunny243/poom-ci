13dec6e50255ee4713eaf047c7ed3567c2132077
nelt <nel@flexio.fr>
2019-03-12 13:52:56 +0100

    upgraded to latest snapshots

--- poom-ci-dependency-gremlin/poom-ci-dependency-gremlin-service/src/test/java/org/codingmatters/poom/ci/gremlin/GremlinExploreTest.java
+++ poom-ci-dependency-gremlin/poom-ci-dependency-gremlin-service/src/test/java/org/codingmatters/poom/ci/gremlin/GremlinExploreTest.java
@@ -2,15 +2,19 @@ package org.codingmatters.poom.ci.gremlin;
 
 import io.flexio.docker.DockerResource;
 import org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
 
 public class GremlinExploreTest {
 
     @ClassRule
     static public final DockerResource docker = GremlinResource.withGremlinContainer(DockerResource.client());
+
+    @Rule
+    public TemporaryFolder tmp = new TemporaryFolder();
 
     @Rule
     public GremlinResource gremlin = new GremlinResource(docker);
@@ -20,19 +24,33 @@ public class GremlinExploreTest {
         TestGraph.setup(AnonymousTraversalSource.traversal().withRemote(this.gremlin.remoteConnection()));
 
 //        GraphTraversalSource g = AnonymousTraversalSource.traversal().withRemote(this.gremlin.remoteConnection());
+//        File out = this.tmp.newFile();
+//        g.io(out.getAbsolutePath()).write().iterate();
+//
+//        System.out.println("#################################################################################");
+//        try(Reader reader = new FileReader(out)) {
+//            char[] buffer = new char[1024];
+//            for(int read = reader.read(buffer) ; read != -1 ; read = reader.read(buffer)) {
+//                System.out.printf("%s", new String(buffer, 0, read));
+//            }
+//        }
+//        System.out.println("#################################################################################");
+
+
+//        GraphTraversalSource g = AnonymousTraversalSource.traversal().withRemote(this.gremlin.remoteConnection());
 //        Vertex repo1 = g.V().hasLabel("repository").has("repository-id", "orga1-repo1-branch1").next();
 //        System.out.printf("repo1 : %s", repo1);
 
 
-        GraphTraversalSource g = AnonymousTraversalSource.traversal().withRemote(this.gremlin.remoteConnection());
-
-        System.out.printf("%s downstreams :: %s\n",
-                "orga-repo1-branch",
-                g.V().hasLabel("repository").has("repository-id", "orga-repo1-branch")
-                        .out("produces").hasLabel("module").in("depends-on").hasLabel("repository").toList());
-        System.out.printf("%s downstreams :: %s\n",
-                "orga-repo5-branch",
-                g.V().hasLabel("repository").has("repository-id", "orga-repo5-branch")
-                        .out("produces").hasLabel("module").in("depends-on").hasLabel("repository").toList());
+//        GraphTraversalSource g = AnonymousTraversalSource.traversal().withRemote(this.gremlin.remoteConnection());
+//
+//        System.out.printf("%s downstreams :: %s\n",
+//                "orga-repo1-branch",
+//                g.V().hasLabel("repository").has("repository-id", "orga-repo1-branch")
+//                        .out("produces").hasLabel("module").in("depends-on").hasLabel("repository").toList());
+//        System.out.printf("%s downstreams :: %s\n",
+//                "orga-repo5-branch",
+//                g.V().hasLabel("repository").has("repository-id", "orga-repo5-branch")
+//                        .out("produces").hasLabel("module").in("depends-on").hasLabel("repository").toList());
     }
 }
