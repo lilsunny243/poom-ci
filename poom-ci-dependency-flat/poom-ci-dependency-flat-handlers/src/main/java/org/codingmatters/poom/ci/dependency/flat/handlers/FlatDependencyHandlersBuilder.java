954cd76af674c9688d7a3253860ace505559e67d
nelt <nel@flexio.fr>
2020-04-21 08:13:00 +0200

    create or update handler
    ref #3

--- poom-ci-dependency-flat/poom-ci-dependency-flat-handlers/src/main/java/org/codingmatters/poom/ci/dependency/flat/handlers/FlatDependencyHandlersBuilder.java
+++ poom-ci-dependency-flat/poom-ci-dependency-flat-handlers/src/main/java/org/codingmatters/poom/ci/dependency/flat/handlers/FlatDependencyHandlersBuilder.java
@@ -8,6 +8,7 @@ public class FlatDependencyHandlersBuilder extends PoomCIDependencyAPIHandlers.B
     public FlatDependencyHandlersBuilder(GraphManager graphManager) {
         this.repositoriesGetHandler(new RepositoryList(graphManager));
         this.repositoryGetHandler(new RepositoryGet(graphManager));
+        this.repositoryPutHandler(new RepositoryCreateOrUpdate(graphManager));
         this.repositoryDeleteHandler(new RepositoryDelete(graphManager));
         this.repositoryDependenciesGetHandler(new RepositoryDependsOn(graphManager));
         this.repositoryModulesGetHandler(new RepositoryProduces(graphManager));
